3df3d16b7f42e92f8b0fe72e7e137932931db707
Sam <sammintz1993@gmail.com>
2019-01-02 15:26:43 -0600

    set up click listeners for rps

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log("Hello World")
+// global variables 
+const userScore = 0;
+const compScore = 0;
+// dom variables. used to store dom elements
+const userScore_span = document.getElementById("user-score");
+const compScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result-message");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+rock_div.addEventListener("click", () => {
+	console.log('hey, you clicked rock')
+});
+
+paper_div.addEventListener("click", () => {
+	console.log('hey, you clicked paper')
+});
+
+scissors_div.addEventListener("click", () => {
+	console.log('hey, you clicked scissors')
+});
